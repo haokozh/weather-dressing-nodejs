4ae5dc561b238dcfae93aae2dd76dd2b8083e0fc
haokozh <peter9617189@gmail.com>
2021-07-07 09:30:43 +0800

    Refactor isMessage(), isTextMessage(), isWebhookTest()

--- index.js
+++ index.js
@@ -3,41 +3,56 @@ const express = require('express');
 const axios = require('axios');
 
 const lineConfig = {
-    channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
-    channelSecret: process.env.CHANNEL_SECRET
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET,
 };
 
 const app = express();
 const client = new line.Client(lineConfig);
 
 app.post('/callback', line.middleware(lineConfig), (req, res) => {
-    Promise.all(req.body.events.map(handleEvent))
-        .then((result) => res.json(result))
-        .catch((err) => {
-            console.error(err);
-            res.status(500).end();
-        });
+  Promise.all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).end();
+    });
 });
 
 function handleEvent(event) {
-    if (event.type !== 'message' || event.message.type !== 'text') {
-        return Promise.resolve(null);
-    }
+  if (
+    !isMessage(event.type) ||
+    !isTextMessage(event.message.type) ||
+    isWebhookTest(event.replyToken)
+  ) {
+    return Promise.resolve(null);
+  }
 
-    if (event.replyToken === '00000000000000000000000000000000' || event.replyToken === 'ffffffffffffffffffffffffffffffff') {
-        return Promise.resolve(null);
-    }
+  const echo = {
+    type: 'text',
+    text: event.message.text,
+  };
 
-    const echo = {
-        type: 'text',
-        text: event.message.text,
-    };
+  return client.replyMessage(event.replyToken, echo);
+}
 
-    return client.replyMessage(event.replyToken, echo);
+function isMessage(eventType) {
+  return eventType === 'message';
+}
+
+function isTextMessage(messageType) {
+  return messageType === 'text';
+}
+
+function isWebhookTest(replyToken) {
+  return (
+    replyToken === '00000000000000000000000000000000' ||
+    replyToken === 'ffffffffffffffffffffffffffffffff'
+  );
 }
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log(`Listening on ${port}`);
+  console.log(`Listening on ${port}`);
 });

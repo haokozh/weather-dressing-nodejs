81cba07220a5597ef89a9c9740ba02da8f9aab46
haokozh <peter9617189@gmail.com>
2021-09-23 01:07:05 +0800

    add bootstrap and add index page

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const getTargetDistByLocationsName = require('./lib/getTargetDistByLocationsName
 const isWebhookTest = require('./lib/isWebhookTest');
 const config = require('./config');
 
+
 const replyText = (token, texts) => {
   texts = Array.isArray(texts) ? texts : [texts];
   return client.replyMessage(
@@ -23,6 +24,11 @@ const client = new line.Client({
   channelAccessToken: config.channelAccessToken,
 });
 
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
 app.post('/callback', line.middleware(client.config), (req, res) => {
   Promise.all(req.body.events.map(handleEvent))
     .then((result) => res.json(result))
@@ -30,6 +36,10 @@ app.post('/callback', line.middleware(client.config), (req, res) => {
       console.error(err);
       res.status(500).end();
     });
+});
+
+app.get('/', (req, res) => {
+  res.render('index');
 });
 
 async function handleEvent(event) {
@@ -41,7 +51,10 @@ async function handleEvent(event) {
   const splitedText = originalText.split(' ');
 
   let locationId = getWeeklyLocationId(splitedText[0]);
-  let locationName = getTargetDistByLocationsName(splitedText[1], splitedText[0]);
+  let locationName = getTargetDistByLocationsName(
+    splitedText[1],
+    splitedText[0]
+  );
 
   replyMessage = await getWeatherResponse(
     locationId,

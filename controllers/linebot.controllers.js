89a0f00b1d6d87849131c28073aced3adfb2070f
haokozh <peter9617189@gmail.com>
2021-09-25 15:16:38 +0800

    import line client in linebot.controllers.js

--- controllers/linebot.controllers.js
+++ controllers/linebot.controllers.js
@@ -1,3 +1,6 @@
+const line = require('@line/bot-sdk');
+const linebotConfig = require('../config/linebot');
+
 const isWebhookTest = require('../lib/isWebhookTest');
 
 const getWeatherResponse = require('../lib/getWeatherResponse');
@@ -5,27 +8,32 @@ const get48HoursLocationId = require('../lib/get48HoursLocationId');
 const getWeeklyLocationId = require('../lib/getWeeklyLocationId');
 const getTargetDistByLocationsName = require('../lib/getTargetDistByLocationsName');
 
-async function handleEvent(event) {
-    if (isWebhookTest(event.replyToken)) return Promise.resolve(null);
-  
-    const elementName = ['MinT', 'MaxT', 'PoP12h', 'Wx', 'MinCI', 'MaxCI'];
-  
-    const originalText = event.message.text;
-    const splitedText = originalText.split(' ');
-  
-    let locationId = getWeeklyLocationId(splitedText[0]);
-    let locationName = getTargetDistByLocationsName(
-      splitedText[1],
-      splitedText[0]
-    );
-  
-    replyMessage = await getWeatherResponse(
-      locationId,
-      locationName,
-      elementName
-    );
-  
-    return client.replyMessage(event.replyToken, replyMessage);
-  }
+const client = new line.Client({
+  channelSecret: linebotConfig.channelSecret,
+  channelAccessToken: linebotConfig.channelAccessToken,
+});
 
-  module.exports.handleEvent = handleEvent;
+async function handleEvent(event) {
+  if (isWebhookTest(event.replyToken)) return Promise.resolve(null);
+
+  const elementName = ['MinT', 'MaxT', 'PoP12h', 'Wx', 'MinCI', 'MaxCI'];
+
+  const originalText = event.message.text;
+  const splitedText = originalText.split(' ');
+
+  let locationId = getWeeklyLocationId(splitedText[0]);
+  let locationName = getTargetDistByLocationsName(
+    splitedText[1],
+    splitedText[0]
+  );
+
+  replyMessage = await getWeatherResponse(
+    locationId,
+    locationName,
+    elementName
+  );
+
+  return client.replyMessage(event.replyToken, replyMessage);
+}
+
+module.exports.handleEvent = handleEvent;

2bf283f8ba1170688fa6ccfe4b42174b07de3a2e
haokozh <peter9617189@gmail.com>
2021-09-28 18:14:33 +0800

    change db config to process.env

--- config/db.js
+++ config/db.js
@@ -1,11 +1,11 @@
 module.exports = {
-  host: 'ec2-35-174-56-18.compute-1.amazonaws.com',
-  port: 5432,
-  user: 'mdjbpabddpzbxs',
-  password: 'edb3cd219cecbfa25b339eab1cd4fe11b8497c93402c6f862a19ecd6d546915f',
-  database: 'd29q5hq9d944m3',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   ssl: {
-    required: true,
-    rejectUnauthorized: false,
+    required: process.env.DB_SSL_MODE,
+    rejectUnauthorized: process.env.REJECT_UNAUTHORIZED,
   },
 };

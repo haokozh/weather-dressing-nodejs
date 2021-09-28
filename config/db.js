faccad8311297625cb9a7e5226e0707f3cb71076
haokozh <peter9617189@gmail.com>
2021-09-28 23:30:32 +0800

    change ssl property

--- config/db.js
+++ config/db.js
@@ -4,8 +4,5 @@ module.exports = {
   user: 'mdjbpabddpzbxs',
   password: 'edb3cd219cecbfa25b339eab1cd4fe11b8497c93402c6f862a19ecd6d546915f',
   database: 'd29q5hq9d944m3',
-  ssl: {
-    sslmode: 'require',
-    rejectUnauthorized: false,
-  },
+  ssl: true
 };

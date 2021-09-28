c0333727df6279ae528588d6dfd9f4f618a3260e
haokozh <peter9617189@gmail.com>
2021-09-28 18:41:54 +0800

    change db config to hard code

--- config/db.js
+++ config/db.js
@@ -1,9 +1,9 @@
 module.exports = {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: 'ec2-35-174-56-18.compute-1.amazonaws.com',
+  port: 5432,
+  user: 'mdjbpabddpzbxs',
+  password: 'edb3cd219cecbfa25b339eab1cd4fe11b8497c93402c6f862a19ecd6d546915f',
+  database: 'd29q5hq9d944m3',
   ssl: {
     required: true,
     rejectUnauthorized: 0,

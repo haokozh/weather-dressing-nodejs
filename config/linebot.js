f86c00220d074516fedc4cfc034462bd27582215
haokozh <peter9617189@gmail.com>
2021-10-03 12:56:23 +0800

    change linebot config to hard code

--- config/linebot.js
+++ config/linebot.js
@@ -1,4 +1,7 @@
 module.exports = {
-  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
-  channelSecret: process.env.CHANNEL_SECRET,
+  // channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  // channelSecret: process.env.CHANNEL_SECRET,
+
+  channelAccessToken: 'I3CfNNchPiSWtkqIlWSLAIqSmfxLjx85bH49M9HKdzwRerM6o6IBlRPjNDL2cHuX23SvDINSK6lNvyPJFf/iDavyapeE6JE8o4AAoHuNmDajXz9dxBlLLzYb0yul+2qk8eFNSMb7Yd2+UYehUK3PgAdB04t89/1O/w1cDnyilFU=',
+  channelSecret: 'a04659f553dae2c22fba4967ba238698',
 };

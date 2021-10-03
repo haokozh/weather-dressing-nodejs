2e329cda0cbacab8ff0c8515dffa50893d70d81d
haokozh <peter9617189@gmail.com>
2021-10-03 17:16:52 +0800

    move res.render() and newMember from members.routes to members.controllers

--- controllers/members.controllers.js
+++ controllers/members.controllers.js
@@ -1,9 +1,27 @@
 const memberService = require('../services/members.services');
 
-const newMember = (account, password, confirmPassword, gender) => {
-  memberService.newMember(account, password, gender);
+const register = (req, res) => {
+  res.render('members/register');
+};
+
+const newMember = (req, res) => {
+  try {
+    console.log('Here is req.body:');
+    console.log(JSON.stringify(req.body));
+
+    memberService.newMember(
+      req.body.account,
+      req.body.password,
+      req.body.gender
+    );
+
+    res.status(200).send(req.body);
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 module.exports = {
+  register,
   newMember,
 };
--- routes/members.routes.js
+++ routes/members.routes.js
@@ -6,26 +6,8 @@ const upload = multer();
 
 const memberController = require('../controllers/members.controllers');
 
-router.get('/register', (req, res) => {
-  res.render('members/register');
-});
+router.get('/register', memberController.register);
 
-router.post('/register', upload.array(), (req, res) => {
-  try {
-    console.log('Here is req.body:');
-    console.log(JSON.stringify(req.body));
-
-    memberController.newMember(
-      req.body.account,
-      req.body.password,
-      req.body.confirmPassword,
-      req.body.gender
-    );
-
-    res.status(200).send(req.body);
-  } catch (error) {
-    console.error(error);
-  }
-});
+router.post('/register', upload.array(), memberController.newMember);
 
 module.exports = router;

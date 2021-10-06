03da5cd2c1ab304ed071d4ffe350556a9ded868f
haokozh <peter9617189@gmail.com>
2021-10-06 23:18:56 +0800

    remove getter setter in member.model

--- models/member.model.js
+++ models/member.model.js
@@ -1,33 +1,9 @@
 class Member {
-    constructor(member) {
-        this.account = member.account;
-        this.password = member.password;
-        this.gender = member.gender;
-    }
-
-    get account() {
-        return this.account;
-    }
-
-    set account(account) {
-        this.account = account;
-    }
-
-    get password() {
-        return this.password;
-    }
-
-    set password(password) {
-        this.password = password;
-    }
-
-    get gender() {
-        return this.gender;
-    }
-
-    set gender(gender) {
-        this.gender = gender;
-    }
+  constructor(member) {
+    this.account = member.account;
+    this.password = member.password;
+    this.gender = member.gender;
+  }
 }
 
 module.exports = Member;

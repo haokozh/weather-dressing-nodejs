217b349cfc464889b6e613eb775f1d23377a08c8
haokozh <peter9617189@gmail.com>
2021-10-10 06:59:56 +0800

    remove comment in response-data.model

--- models/response-data.model.js
+++ models/response-data.model.js
@@ -4,13 +4,6 @@ class ResponseData {
 
     this.locationsName = this.getLocations(0).locationsName;
     this.locationName = this.getLocation(0).locationName;
-
-    // this.pop12hDescription = this.getWeatherElement(0, 0).description;
-    // this.minCIDescription = this.getWeatherElement(0, 1).description;
-    // this.weatherDescription = this.getWeatherElement(0, 2).description;
-    // this.maxCIDescription = this.getWeatherElement(0, 3).description;
-    // this.minTDescription = this.getWeatherElement(0, 4).description;
-    // this.maxTDescription = this.getWeatherElement(0, 5).description;
   }
 
   getLocations(locationIndex) {

31b34d7801f7ad998719f0198e52a57ba6020f23
haokozh <peter9617189@gmail.com>
2021-10-13 21:03:57 +0800

    change flex message image

--- services/linebot.service.js
+++ services/linebot.service.js
@@ -663,7 +663,7 @@ const replyFlexBubble = (
       type: 'bubble',
       hero: {
         type: 'image',
-        url: 'https://picsum.photos/200',
+        url: '../public/assets/images/weather-dressing-logo.jpg',
         size: 'full',
         aspectRatio: '20:13',
         aspectMode: 'cover',

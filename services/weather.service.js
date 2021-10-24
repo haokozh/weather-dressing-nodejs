50b22ce4f13e0c2f6d4458866706f09d269ce675
haokozh <peter9617189@gmail.com>
2021-10-24 23:37:01 +0800

    change to .weeklyid

--- services/weather.service.js
+++ services/weather.service.js
@@ -267,7 +267,7 @@ const replyWeather = async (cityName, distName) => {
   try {
     const forecastId = findWeeklyIdByCityName(cityName)[0];
 
-    await getCWBResponse('F-D0047-063', distName).then((res) => {
+    await getCWBResponse(forecastId.weeklyid, distName).then((res) => {
       return parseResponse(res.records, cityName, distName);
     });
   } catch (error) {

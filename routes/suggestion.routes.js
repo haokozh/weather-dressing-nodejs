6754e6ee2ca7cfff01e8e41f8bd8ceac281fb810
haokozh <peter9617189@gmail.com>
2021-10-25 20:34:06 +0800

    change css, js path and test href

--- routes/suggestion.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const suggestionController = require('../controllers/suggestion.controller');
-
-router.get('/', suggestionController.suggestion);
-router.get('/select', suggestionController.select);
-
-module.exports = router;

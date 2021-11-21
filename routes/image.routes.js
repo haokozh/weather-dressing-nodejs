017907d7c3347e2268793b311273c2ba92405024
haokozh <peter9617189@gmail.com>
2021-11-22 00:46:12 +0800

    import imageController in image.routes

--- routes/image.routes.js
+++ routes/image.routes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const imageController = require('../controllers/image.controller');
+
 router.get('/dressStyle/one/1040', imageController.dressStyleOne);
 
 module.exports = router;

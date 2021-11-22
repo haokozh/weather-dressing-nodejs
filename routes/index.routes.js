c044b0d6c9fd4ec1c816da60b789132be194842c
haokozh <peter9617189@gmail.com>
2021-11-22 17:12:25 +0800

    change upload.single() argument

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -21,6 +21,6 @@ const indexController = require('../controllers/index.controller');
 router.get('/', indexController.index);
 router.get('/about', indexController.about);
 router.get('/upload', indexController.renderUploadImage);
-router.post('/upload', upload.single('upload'), indexController.uploadImage);
+router.post('/upload', upload.single('picture'), indexController.uploadImage);
 
 module.exports = router;

6e45e9f909a104862c0b83f4fa7c9f728d62107a
haokozh <peter9617189@gmail.com>
2021-11-22 21:05:48 +0800

    change upload destination

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -2,14 +2,8 @@ const express = require('express');
 const multer = require('multer');
 const path = require('path');
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'uploads');
-  },
-  filename: (req, file, cb) => {
-    console.log(file);
-    cb(null, Date.now() + path.extname(file.originalname));
-  },
+const storage = multer({
+  dest: '/uploads'
 });
 
 const upload = multer({ storage: storage });

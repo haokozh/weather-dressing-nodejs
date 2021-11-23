7d344f2290a34d4193d9f61974716f7664aa33fe
haokozh <peter9617189@gmail.com>
2021-11-23 23:09:48 +0800

    add image url

--- controllers/image.controller.js
+++ controllers/image.controller.js
@@ -40,14 +40,49 @@ const beams = (req, res) => {
   res.sendFile(`${IMAGE_BASE_URL}/logo/beams-logo.png`);
 };
 const bershka = (req, res) => {
-  res.sendFile(`${IMAGE_BASE_URL}/logo/bershka-logo.png`);
+  res.sendFile(`${IMAGE_BASE_URL}/logo/bershka-logo.jpg`);
 };
 const burberry = (req, res) => {
-  res.sendFile(`${IMAGE_BASE_URL}/logo/burberry-logo.png`);
+  res.sendFile(`${IMAGE_BASE_URL}/logo/burberry-logo.jpg`);
 };
 const celine = (req, res) => {
-  res.sendFile(`${IMAGE_BASE_URL}/logo/celine-logo.png`);
+  res.sendFile(`${IMAGE_BASE_URL}/logo/celine-logo.jpg`);
 };
+
+const champion = (req, res) => {
+  res.sendFile(`${IMAGE_BASE_URL}/logo/champion-logo.jpg`);
+};
+const chanel = (req, res) => {
+  res.sendFile(`${IMAGE_BASE_URL}/logo/chanel-logo.jpg`);
+};
+const chuu = (req, res) => {
+  res.sendFile(`${IMAGE_BASE_URL}/logo/chuu-logo.jpg`);
+};
+const clubmonaco = (req, res) => {
+  res.sendFile(`${IMAGE_BASE_URL}/logo/clubmonaco-logo.png`);
+};
+
+const coach = (req, res) => {
+  res.sendFile(`${IMAGE_BASE_URL}/logo/coach-logo.jpg`);
+};
+
+const coen = (req, res) => {
+  res.sendFile(`${IMAGE_BASE_URL}/logo/coen-logo.jpg`);
+};
+
+const converse = (req, res) => {
+  res.sendFile(`${IMAGE_BASE_URL}/logo/converse-logo.jpg`);
+};
+
+const darkvictory = (req, res) => {
+  res.sendFile(`${IMAGE_BASE_URL}/logo/darkvictory-logo.png`);
+};
+
+const dior = (req, res) => {
+  res.sendFile(`${IMAGE_BASE_URL}/logo/dior-logo.jpg`);
+};
+
+
 
 module.exports = {
   dressStyleOne1040,
@@ -63,4 +98,13 @@ module.exports = {
   bershka,
   burberry,
   celine,
+  champion,
+  chanel,
+  chuu,
+  clubmonaco,
+  coach,
+  coen,
+  converse,
+  darkvictory,
+  dior,
 };
--- routes/image.routes.js
+++ routes/image.routes.js
@@ -17,6 +17,13 @@ router.get('/carousel/beams', imageController.beams);
 router.get('/carousel/bershka', imageController.bershka);
 router.get('/carousel/burberry', imageController.burberry);
 router.get('/carousel/celine', imageController.celine);
-
-
+router.get('/carousel/champion', imageController.champion);
+router.get('/carousel/chanel', imageController.chanel);
+router.get('/carousel/chuu', imageController.chuu);
+router.get('/carousel/clubmonaco', imageController.clubmonaco);
+router.get('/carousel/coach', imageController.coach);
+router.get('/carousel/coen', imageController.coen);
+router.get('/carousel/converse', imageController.converse);
+router.get('/carousel/darkvictory', imageController.darkvictory);
+router.get('/carousel/dior', imageController.dior);
 module.exports = router;

00b1356430f65c06327e83f0c721f435dec200ee
haokozh <peter9617189@gmail.com>
2021-11-24 00:13:06 +0800

    add image url

--- controllers/image.controller.js
+++ controllers/image.controller.js
@@ -82,7 +82,229 @@ const dior = (req, res) => {
   res.sendFile(`${IMAGE_BASE_URL}/logo/dior-logo.jpg`);
 };
 
+const fila = (req, res) => {
+  res.sendFile(`${IMAGE_BASE_URL}/logo/fila-logo.jpg`);
+};
 
+const g2000 = (req, res) => {
+  res.sendFile(`${IMAGE_BASE_URL}/logo/g2000-logo.jpg`);
+};
+
+const gap = (req, res) => {
+  res.sendFile(`${IMAGE_BASE_URL}/logo/gap-logo.png`);
+};
+
+const genquo = (req, res) => {
+  res.sendFile(`${IMAGE_BASE_URL}/logo/genquo-logo.png`);
+};
+
+const gievesandhawkes = (req, res) => {
+  res.sendFile(`${IMAGE_BASE_URL}/logo/gieves&hawkes-logo.jpg`);
+};
+
+const globalwork = (req, res) => {
+  res.sendFile(`${IMAGE_BASE_URL}/logo/globalwork-logo.jpg`);
+};
+
+const goopi = (req, res) => {
+  res.sendFile(`${IMAGE_BASE_URL}/logo/goopi-logo.png`);
+};
+
+const gu = (req, res) => {
+  res.sendFile(`${IMAGE_BASE_URL}/logo/gu-logo.jpg`);
+};
+
+const gucci = (req, res) => {
+  res.sendFile(`${IMAGE_BASE_URL}/logo/gucci-logo.jpg`);
+};
+
+const guerrillagroup = (req, res) => {
+  res.sendFile(`${IMAGE_BASE_URL}/logo/guerrilla-group-logo.jpg`);
+};
+
+const handm = (req, res) => {
+  res.sendFile(`${IMAGE_BASE_URL}/logo/h&m-logo.png`);
+};
+
+const hotping = (req, res) => {
+  res.sendFile(`${IMAGE_BASE_URL}/logo/hotping-logo.png`);
+};
+
+const ifandn = (req, res) => {
+  res.sendFile(`${IMAGE_BASE_URL}/logo/if&n-logo.jpg`);
+};
+
+const ig = (req, res) => {
+  res.sendFile(`${IMAGE_BASE_URL}/logo/ig-logo.jpg`);
+};
+
+const lativ = (req, res) => {
+  res.sendFile(`${IMAGE_BASE_URL}/logo/lativ-logo.png`);
+};
+
+const lovfee = (req, res) => {
+  res.sendFile(`${IMAGE_BASE_URL}/logo/lovfee-logo.jpg`);
+};
+
+const lowrysfarm = (req, res) => {
+  res.sendFile(`${IMAGE_BASE_URL}/logo/lowrysfarm-logo.jpg`);
+};
+
+const maddness = (req, res) => {
+  res.sendFile(`${IMAGE_BASE_URL}/logo/maddness-logo.jpg`);
+};
+
+const massimodutti = (req, res) => {
+  res.sendFile(`${IMAGE_BASE_URL}/logo/massimodutti-logo.jpg`);
+};
+
+const meierdotq = (req, res) => {
+  res.sendFile(`${IMAGE_BASE_URL}/logo/meier.q-logo.jpg`);
+};
+
+const michaelkors = (req, res) => {
+  res.sendFile(`${IMAGE_BASE_URL}/logo/michaelkors-logo.jpg`);
+};
+
+const mouggan = (req, res) => {
+  res.sendFile(`${IMAGE_BASE_URL}/logo/mouggan-logo.jpg`);
+};
+
+const nb = (req, res) => {
+  res.sendFile(`${IMAGE_BASE_URL}/logo/nb-logo.png`);
+};
+
+const nerdy = (req, res) => {
+  res.sendFile(`${IMAGE_BASE_URL}/logo/nerdy-logo.jpg`);
+};
+
+const nikoand = (req, res) => {
+  res.sendFile(`${IMAGE_BASE_URL}/logo/nikoand-logo.jpg`);
+};
+
+const ninetheory = (req, res) => {
+  res.sendFile(`${IMAGE_BASE_URL}/logo/ninetheory-logo.jpg`);
+};
+
+const ob = (req, res) => {
+  res.sendFile(`${IMAGE_BASE_URL}/logo/ob-logo.jpg`);
+};
+
+const offwhite = (req, res) => {
+  res.sendFile(`${IMAGE_BASE_URL}/logo/offwhite-logo.jpg`);
+};
+
+const oneboy = (req, res) => {
+  res.sendFile(`${IMAGE_BASE_URL}/logo/oneboy-logo.jpg`);
+};
+
+const oqliq = (req, res) => {
+  res.sendFile(`${IMAGE_BASE_URL}/logo/oqliq-logo.jpg`);
+};
+
+const pazzo = (req, res) => {
+  res.sendFile(`${IMAGE_BASE_URL}/logo/pazzo.jpg`);
+};
+
+const phantaci = (req, res) => {
+  res.sendFile(`${IMAGE_BASE_URL}/logo/phantaci-logo.jpg`);
+};
+
+const professordote = (req, res) => {
+  res.sendFile(`${IMAGE_BASE_URL}/logo/professor.e-logo.png`);
+};
+
+const pullandbear = (req, res) => {
+  res.sendFile(`${IMAGE_BASE_URL}/logo/pull&bear-logo.jpg`);
+};
+
+const puma = (req, res) => {
+  res.sendFile(`${IMAGE_BASE_URL}/logo/puma-logo.jpg`);
+};
+
+const queenshop = (req, res) => {
+  res.sendFile(`${IMAGE_BASE_URL}/logo/queenshop-logo.jpg`);
+};
+
+const sealson = (req, res) => {
+  res.sendFile(`${IMAGE_BASE_URL}/logo/season-logo.jpg`);
+};
+
+const spao = (req, res) => {
+  res.sendFile(`${IMAGE_BASE_URL}/logo/spao-logo.jpg`);
+};
+
+const sstandc = (req, res) => {
+  res.sendFile(`${IMAGE_BASE_URL}/logo/sst&c-logo.jpg`);
+};
+
+const stayreal = (req, res) => {
+  res.sendFile(`${IMAGE_BASE_URL}/logo/stayreal-logo.jpg`);
+};
+
+const stussy = (req, res) => {
+  res.sendFile(`${IMAGE_BASE_URL}/logo/stussy-logo.jpg`);
+};
+
+const suitangtang = (req, res) => {
+  res.sendFile(`${IMAGE_BASE_URL}/logo/suitangtang-logo.jpg`);
+};
+
+const suitsupply = (req, res) => {
+  res.sendFile(`${IMAGE_BASE_URL}/logo/suitsupply-logo.png`);
+};
+
+const superdry = (req, res) => {
+  res.sendFile(`${IMAGE_BASE_URL}/logo/superdry-logo.jpg`);
+};
+
+const supreme = (req, res) => {
+  res.sendFile(`${IMAGE_BASE_URL}/logo/supreme-logo.jpg`);
+};
+
+const syndro = (req, res) => {
+  res.sendFile(`${IMAGE_BASE_URL}/logo/syndro-logo.jpg`);
+};
+
+const th = (req, res) => {
+  res.sendFile(`${IMAGE_BASE_URL}/logo/th-logo.jpg`);
+};
+
+const tiffany = (req, res) => {
+  res.sendFile(`${IMAGE_BASE_URL}/logo/tiffany-logo.jpg`);
+};
+
+const ua = (req, res) => {
+  res.sendFile(`${IMAGE_BASE_URL}/logo/ua-logo.jpg`);
+};
+
+const uq = (req, res) => {
+  res.sendFile(`${IMAGE_BASE_URL}/logo/uq-logo.jpg`);
+};
+
+const ur = (req, res) => {
+  res.sendFile(`${IMAGE_BASE_URL}/logo/ur-logo.png`);
+};
+
+const vans = (req, res) => {
+  res.sendFile(`${IMAGE_BASE_URL}/logo/vans-logo.jpg`);
+};
+
+const wd = (req, res) => {
+  res.sendFile(`${IMAGE_BASE_URL}/logo/wd-logo.png`);
+};
+
+const wisdom = (req, res) => {
+  res.sendFile(`${IMAGE_BASE_URL}/logo/wisdom-logo.jpg`);
+};
+
+const ysl = (req, res) => {
+  res.sendFile(`${IMAGE_BASE_URL}/logo/ysl-logo.jpg`);
+};
+
+const zara = (req, res) => {
+  res.sendFile(`${IMAGE_BASE_URL}/logo/zara-logo.jpg`);
+};
 
 module.exports = {
   dressStyleOne1040,
@@ -107,4 +329,60 @@ module.exports = {
   converse,
   darkvictory,
   dior,
+  fila,
+  g2000,
+  gap,
+  genquo,
+  gievesandhawkes,
+  globalwork,
+  goopi,
+  gu,
+  gucci,
+  guerrillagroup,
+  handm,
+  hotping,
+  ifandn,
+  ig,
+  lativ,
+  lovfee,
+  lowrysfarm,
+  maddness,
+  massimodutti,
+  meierdotq,
+  michaelkors,
+  mouggan,
+  nb,
+  nerdy,
+  nikoand,
+  ninetheory,
+  ob,
+  offwhite,
+  oneboy,
+  oqliq,
+  pazzo,
+  phantaci,
+  professordote,
+  pullandbear,
+  puma,
+  queenshop,
+  sealson,
+  spao,
+  sstandc,
+  stayreal,
+  stussy,
+  suitangtang,
+  suitsupply,
+  superdry,
+  supreme,
+  syndro,
+  th,
+  tiffany,
+  ua,
+  uq,
+  ur,
+  vans,
+  wd,
+  wisdom,
+  ysl,
+  zara,
 };

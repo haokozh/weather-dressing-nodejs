ac41ea601c4c989aea60b8105a34b69337f16f4d
haokozh <peter9617189@gmail.com>
2021-11-23 17:56:20 +0800

    test image url

--- controllers/image.controller.js
+++ controllers/image.controller.js
@@ -1,21 +1,27 @@
+const IMAGE_BASE_URL = '/app/public/assets/images';
+
 const dressStyleOne1040 = (req, res) => {
-  res.sendFile('/app/public/assets/images/imagemap/dress-style-1040.png');
+  res.sendFile(`${IMAGE_BASE_URL}/imagemap/dress-style-1040.png`);
 };
 
 const dressStyleOne700 = (req, res) => {
-  res.sendFile('/app/public/assets/images/imagemap/dress-style-700.png');
+  res.sendFile(`${IMAGE_BASE_URL}/imagemap/dress-style-700.png`);
 };
 
 const dressStyleTwo1040 = (req, res) => {
-  res.sendFile('/app/public/assets/images/imagemap/dress-style-1040-2.png');
+  res.sendFile(`${IMAGE_BASE_URL}/imagemap/dress-style-1040-2.png`);
 };
 
 const dressStyleTwo700 = (req, res) => {
-  res.sendFile('/app/public/assets/images/imagemap/dress-style-700-2.png');
+  res.sendFile(`${IMAGE_BASE_URL}/imagemap/dress-style-700-2.png`);
 };
 
 const nike = (req, res) => {
-  res.sendFile('/app/public/assets/images/logo/nike-logo.jpg');
+  res.sendFile(`${IMAGE_BASE_URL}/logo/nike-logo.jpg`);
+};
+
+const adidas = (req, res) => {
+  res.sendFile(`${IMAGE_BASE_URL}/logo/`);
 };
 
 module.exports = {
@@ -24,4 +30,5 @@ module.exports = {
   dressStyleTwo1040,
   dressStyleTwo700,
   nike,
+  adidas,
 };
--- routes/image.routes.js
+++ routes/image.routes.js
@@ -7,6 +7,10 @@ router.get('/dressStyle/one/1040', imageController.dressStyleOne1040);
 router.get('/dressStyle/one/700', imageController.dressStyleOne700);
 router.get('/dressStyle/two/1040', imageController.dressStyleTwo1040);
 router.get('/dressStyle/two/700', imageController.dressStyleTwo700);
+
 router.get('/carousel/nike', imageController.nike);
+router.get('/carousel/adidas', imageController.adidas);
+router.get('/carousel/66girls', imageController.girls66);
+router.get('')
 
 module.exports = router;

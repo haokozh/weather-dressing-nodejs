c111f84ae4e23ca76522aeabd2432f54e288bb0e
haokozh <peter9617189@gmail.com>
2021-11-25 22:12:33 +0800

    test multer destination

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'uploads');
+    cb(null, 'public/uploads');
   },
   filename: (req, file, cb) => {
     console.log(file);

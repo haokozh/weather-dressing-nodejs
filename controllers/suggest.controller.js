afc58fbad854135b206262380475aa45ce54736c
haokozh <peter9617189@gmail.com>
2021-11-27 14:49:27 +0800

    update dresslist and add final.ejs

--- controllers/suggest.controller.js
+++ controllers/suggest.controller.js
@@ -2,6 +2,11 @@ const suggestion = (req, res) => {
   res.render('weather/suggestion', { title: '穿搭建議' });
 };
 
+const final = (req, res) => {
+  res.render('weather/final', { title: '建議結果' });
+};
+
 module.exports = {
   suggestion,
+  final,
 };
--- routes/suggest.routes.js
+++ routes/suggest.routes.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const suggestController = require('../controllers/suggest.controller');
 
 router.get('/suggestion', suggestController.suggestion);
+router.get('/final', suggestController.final);
 
 module.exports = router;

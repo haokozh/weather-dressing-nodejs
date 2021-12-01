77326be2e81bd0f25322de29105470fb85b5d6bd
haokozh <peter9617189@gmail.com>
2021-12-01 19:11:05 +0800

    add suggestion

--- routes/suggest.routes.js
+++ routes/suggest.routes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const upload = multer();
 
 const suggestController = require('../controllers/suggest.controller');
 
 router.get('/suggestion', suggestController.suggestion);
-router.post('/suggestion', suggestController.sendSuggestion);
+router.post('/suggestion', upload.none(), suggestController.sendSuggestion);
 router.get('/final', suggestController.final);
 
 module.exports = router;

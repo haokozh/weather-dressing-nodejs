832847543230608f5ec740cddb53d66dda98075d
haokozh <peter9617189@gmail.com>
2021-12-08 16:05:56 +0800

    add acl and contentType

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -9,11 +9,13 @@ const s3 = new aws.S3();
 const storage = multerS3({
   s3: s3,
   bucket: 'weather-dressing-assets',
+  contentType: multerS3.AUTO_CONTENT_TYPE,
+  acl: 'public-read-write',
   metadata: (req, file, cb) => {
     cb(null, { fieldName: file.fieldname });
   },
   key: (req, file, cb) => {
-    cb(null, Date.now().toString());
+    cb(null, new Date().toISOString());
   },
 });
 
